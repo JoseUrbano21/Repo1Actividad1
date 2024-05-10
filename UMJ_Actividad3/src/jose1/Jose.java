b3d7dc7129becc73e1b81c12e5a62f298ea06d0d
Jose Urbano <joseurbano2192@gmail.com>
2024-05-10 21:22:51 +0200

    Método creado Jose1

---
+++ UMJ_Actividad3/src/jose1/Jose.java
@@ -0,0 +1,15 @@
+package jose1;
+
+public class Jose {
+
+	public static void main(String[] args) {
+
+		miNombre();
+
+	}
+	
+	public static void miNombre() {
+		System.out.println("Hola, soy Jose1");
+	}
+
+}
