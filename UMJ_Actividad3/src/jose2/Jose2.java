27065abb8ec25570700449940a2c16e499175874
Alvaro <alvarovr712@gmail.com>
2024-05-10 21:22:53 +0200

    Jose1 version1

---
+++ UMJ_Actividad3/src/jose2/Jose2.java
@@ -0,0 +1,19 @@
+package jose2;
+
+public class Jose2 {
+
+	public static void main(String[] args) {
+		
+		
+		miNombre();
+		
+		
+	}
+	
+	public static void miNombre() {
+		System.out.println("hola soy jose2");
+	
+	
+		
+	}
+}
